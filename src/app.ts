bb7f10c9389c6a55472545afe59324642e95d6a9
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2024-10-14 18:26:58 +0200

    feat: added cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,18 @@ import authRoutes from './routes/authRoutes';
 import { connectDB } from './database/database';
 import cookieParser from 'cookie-parser';
 import dotenvFlow from 'dotenv-flow';
+import cors from 'cors';
 
 dotenvFlow.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+// Configurar CORS para permitir solicitudes desde diferentes orígenes
+app.use(cors({
+    origin: 'http://localhost:5173', // Define el dominio permitido (por ejemplo, tu frontend)
+    credentials: true, // Para permitir el envío de cookies con la solicitud
+}));
 
 // Middleware
 app.use(express.json());
