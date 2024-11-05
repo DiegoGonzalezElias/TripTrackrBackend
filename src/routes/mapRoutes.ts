fa1dc256e7e6c22f8c87e8dcd8fae1465f4f71e7
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2024-11-05 19:43:01 +0100

    fix: delete map controller fixed

--- src/routes/mapRoutes.ts
+++ src/routes/mapRoutes.ts
@@ -5,7 +5,7 @@ import { createMap, deleteMap, getMarkers, updateMap } from '../map/contollers';
 const router = Router();
 
 router.post('/create-map', requireAuth, createMap);
-router.post('/delete-map', requireAuth, deleteMap);
+router.delete('/delete-map', requireAuth, deleteMap);
 router.post('/update-map', requireAuth, updateMap);
 router.get('/get-all-markers', requireAuth, getMarkers);
 
